27188baf5fe1999247c8b33f6ea58dc7b3cd8d40
M Squared <birthofgenesis@yahoo.com>
2017-11-08 17:05:27 +0000

    Update decimal to binary conversion.cpp

--- Bitset/decimal to binary conversion.cpp
+++ Bitset/decimal to binary conversion.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    auto n = 0;
     cin >> n;
     string binary = bitset<8>(n).to_string(); //for 8 bit representation
     cout << binary << "\n";
