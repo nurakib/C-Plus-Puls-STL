23aa7a362d01cf0b68619aa8e67841c6d4ee2f83
M Squared <birthofgenesis@yahoo.com>
2017-11-08 17:04:40 +0000

    Update partial_sum.cpp

--- Numeric/partial_sum.cpp
+++ Numeric/partial_sum.cpp
@@ -11,19 +11,19 @@ int main(){
 
     partial_sum(val, val+5, result);
     cout << "using default partial_sum: ";
-    for(int i = 0; i < 5; i++)
+    for(auto i = 0; i < 5; i++)
         cout << result[i] << " ";
     cout << endl;
 
     partial_sum(val, val + 5, result, multiplies<int>());
     cout << "using functional operation multiplies: ";
-    for(int i = 0; i < 5; i++)
+    for(auto i = 0; i < 5; i++)
         cout << result[i] << " ";
     cout << endl;
 
     partial_sum(val, val + 5, result, mychoice);
     cout << "using custom function: ";
-    for(int i = 0; i < 5; i++)
+    for(auto i = 0; i < 5; i++)
         cout << result[i] << " ";
     cout << endl;
     
